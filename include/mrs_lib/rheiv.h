6e9a50a4d1927c16a4213c227666732656937757
Matouš Vrba <vrbamato@fel.cvut.cz>
2019-09-20 22:07:47 +0200

    RHEIV: beautified the class, added documentation and some foolproofing

--- include/mrs_lib/rheiv.h
+++ include/mrs_lib/rheiv.h
@@ -8,7 +8,6 @@
 #define HEIV_H
 
 #include <Eigen/Dense>
-#include <iostream>
 
 namespace mrs_lib
 {
@@ -17,99 +16,222 @@ namespace mrs_lib
   /**
   * \brief Implementation of the Reduced Heteroscedastic Errors In Variables surface fitting algorithm \cite FNSandHEIV.
   *
+  * This class estimates a vector of parameters \f$ \mathbf{\theta} \f$ of a model, which is defined by an equation
+  * \f$ \mathbf{\theta}^T \mathbf{u}\left( \mathbf{x} \right) = 0 \f$,
+  * where \f$ \mathbf{x} \f$ is a data sample vector and \f$ \mathbf{u}\left( \mathbf{x} \right) \f$ is a vector,
+  * obtained by transforming \f$ \mathbf{x} \f$ in a problem-dependent manner.
+  *
+  * Note that \f$ \mathbf{u}\left( \mathbf{x} \right) \f$ must fulfill two conditions:
+  * - each element of \f$ \mathbf{u}\left( \mathbf{x} \right) \f$ is a quadratic form of a vector \f$ \left[ \mathbf{x}, 1 \right] \f$, and
+  * - the last element of \f$ \mathbf{u}\left( \mathbf{x} \right) \f$ is equal to one.
+  *
+  * Such model can be used to describe e.g. various surfaces, such as planes or conics, which makes this class a useful tool for computer vision.
+  * The model is described in the context of the RHEIV class by the length of the vector \f$ \mathbf{x} \f$, the length of the vector \f$ \mathbf{\theta} \f$,
+  * and the Jacobian matrix of \f$ \partial_{\mathbf{x}} \mathbf{z}\left( \mathbf{x} \right) \f$, where \f$ \mathbf{z}\left( \mathbf{x} \right) \f$ is defined
+  * by the formula \f$ \mathbf{u}\left( \mathbf{x} \right) = \left[ \mathbf{z}\left( \mathbf{x} \right), 1 \right] \f$.
+  *
+  * For more information, see \cite FNSandHEIV.
+  *
   * Example usage:
-  * \include src/heiv/example.cpp
+  * \include src/rheiv/example.cpp
   *
   */
   template <int n_states, int n_params>
   class RHEIV
   {
+    static_assert(n_states <= n_params, "Sorry, n_states must be smaller or equal to n_params");
   public:
     /* RHEIV definitions (typedefs, constants etc) //{ */
 
-    static const int k = n_states;            /*!< \brief Length of the state vector of the system. */
-    static const int l = n_params;            /*!< \brief Length of the state vector of the system. */
-    static const int lr = l - 1;            /*!< \brief Length of the input vector of the system. */
+    static const int k = n_states;            /*!< \brief Length of the state vector \p x */
+    static const int l = n_params;            /*!< \brief Length of the parameter vector \f$ \mathbf{\theta} \f$ */
+    static const int lr = l - 1;              /*!< \brief Length of the reduced parameter vector \f$ \mathbf{\eta} \f$. */
 
-    using x_t = Eigen::Matrix<double, k, 1>;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using P_t = Eigen::Matrix<double, k, k>;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using Ps_t = std::vector<P_t>;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using dzdx_t = Eigen::Matrix<double, lr, k>;                /*!< \brief Input vector type \f$m \times 1\f$ */
+    using x_t = Eigen::Matrix<double, k, 1>;                /*!< \brief Input vector type \f$k \times 1\f$ */
+    using xs_t = Eigen::Matrix<double, k, -1>;              /*!< \brief Container type for the input data array */
+    using P_t = Eigen::Matrix<double, k, k>;                /*!< \brief Covariance type of the input vector \f$k \times k\f$ */
+    using Ps_t = std::vector<P_t>;                          /*!< \brief Container type for covariances \p P of the input data array */
 
-    using u_t = Eigen::Matrix<double, l, 1>;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using us_t = Eigen::Matrix<double, l, -1>;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using z_t = Eigen::Matrix<double, lr, 1>;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using zs_t = Eigen::Matrix<double, lr, -1>;                /*!< \brief Input vector type \f$m \times 1\f$ */
+    using z_t = Eigen::Matrix<double, lr, 1>;               /*!< \brief Type of a reduced transformed input vector \f$l_r \times 1\f$ */
+    using zs_t = Eigen::Matrix<double, lr, -1>;             /*!< \brief Container type for an array of the reduced transformed input vectors \p z */
 
-    using theta_t = u_t;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using nu_t = z_t;                /*!< \brief Input vector type \f$m \times 1\f$ */
+    using f_z_t = typename std::function<zs_t(const xs_t&)>;        /*!< \brief Function signature of the \f$ \mathbf{z}\left( \mathbf{x} \right) \f$ mapping function */
+    using dzdx_t = Eigen::Matrix<double, lr, k>;                    /*!< \brief Type of the jacobian matrix \f$ \mathbf{J} \mathbf{z}\left( \mathbf{x} \right) \f$, \f$l_r \times k\f$ */
 
-    using A_t = Eigen::Matrix<double, lr, lr>;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using B_t = A_t;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using Bs_t = std::vector<B_t>;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using M_t = A_t;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using N_t = A_t;                /*!< \brief Input vector type \f$m \times 1\f$ */
-    using betas_t = Eigen::Matrix<double, 1, -1>;
+    using theta_t = Eigen::Matrix<double, l, 1>;            /*!< \brief Parameter vector type \f$l \times 1\f$ */
+    using eta_t = z_t;                                      /*!< \brief Reduced parameter vector type \f$l_r \times 1\f$ */
 
+  /*!
+    * \brief This exception may be thrown when solving the generalized eigenvalue problem with the M and N matrices.
+    *
+    * You should catch this exception in your code and act accordingly if it appears
+    * (e.g. ask for the last valid parameter estimate).
+    */
+    struct eigenvector_exception : public std::exception
+    {
+    /*!
+      * \brief Returns the error message, describing what caused the exception.
+      *
+      * \return  The error message, describing what caused the exception.
+      */
+      const char* what() const throw()
+      {
+        return "RHEIV: could not compute matrix eigenvectors! The input data is not well formed.";
+      }
+    };
+
+  private:
+    using A_t = Eigen::Matrix<double, lr, lr>;          /*!< \brief Type of the helper matrix \f$m \mathbf{A} 1\f$, \f$l_r \times l_r\f$ */
+    using B_t = A_t;                                    /*!< \brief Type of the helper matrix \f$m \mathbf{B} 1\f$, \f$l_r \times l_r\f$ */
+    using Bs_t = std::vector<B_t>;                      /*!< \brief Container type for an array of matrices \p B, corresponding to the input data */
+    using M_t = A_t;                                    /*!< \brief Type of the matrix \f$m \mathbf{M} 1\f$, used in the generalized eigen vector problem, \f$l_r \times l_r\f$ */
+    using N_t = A_t;                                    /*!< \brief Type of the matrix \f$m \mathbf{N} 1\f$, used in the generalized eigen vector problem, \f$l_r \times l_r\f$ */
+    using betas_t = Eigen::Matrix<double, 1, -1>;       /*!< \brief Container type for an array of coefficients \p beta, corresponding to the input data */
+    
     //}
 
   public:
-  /*!
-    * \brief Convenience default constructor.
-    *
-    * This constructor should not be used if applicable. If used, the main constructor has to be called afterwards,
-    * before using this class, otherwise the LKF object is invalid (not initialized).
-    */
-    RHEIV(){};
+      /* constructor //{ */
+      /*!
+        * \brief Convenience default constructor.
+        *
+        * \warning This constructor should not be used if applicable. If used, the main constructor has to be called afterwards,
+        *          before using this class, otherwise the RHEIV object is invalid (not initialized).
+        */
+        RHEIV()
+          :
+            m_initialized(false),
+            m_ALS_theta_set(false),
+            m_last_theta_set(false)
+        {};
 
-  /*!
-    * \brief The main constructor.
-    *
-    * \param A             The state transition matrix.
-    */
-    RHEIV(const dzdx_t& dzdx, const double min_dnu = 1e-15, const unsigned max_its = 100) : m_dzdx(dzdx), m_min_dnu(min_dnu), m_max_its(max_its) {};
+      /*!
+        * \brief The main constructor.
+        *
+        * The \p dzdx parameter gives the relation between \f$ \mathbf{z}\left( \mathbf{x} \right) \f$ and \f$ \mathbf{x} \f$. It is the full jacobian matrix
+        * \f$ \partial_{\mathbf{x}} \mathbf{z}\left( \mathbf{x} \right) \f$.
+        *
+        * The optimization algorithm is iterative with two possible stopping conditions:
+        * - if change of the estimate of \f$ \mathbf{\theta} \f$ between two iterations is smaller than a defined threshold, or
+        * - if a maximal number of iterations was reached.
+        *
+        * \param f_z        the mapping function \f$ \mathbf{z}\left( \mathbf{x} \right) \f$.
+        * \param dzdx       the jacobian matrix of partial derivations of \f$ \mathbf{z}\left( \mathbf{x} \right) \f$ by \f$ \mathbf{x} \f$.
+        * \param min_dtheta   if the difference of \f$ \mathbf{\theta}_{k} \f$ and \f$ \mathbf{\theta}_{k-1} \f$ is smaller than this number, the iteration is stopped.
+        * \param max_its    if the iteration is stopped after this number of iterations.
+        */
+        RHEIV(const f_z_t& f_z, const dzdx_t& dzdx, const double min_dtheta = 1e-15, const unsigned max_its = 100)
+          : 
+            m_initialized(true),
+            m_f_z(f_z),
+            m_dzdx(dzdx),
+            m_min_dtheta(min_dtheta),
+            m_max_its(max_its),
+            m_ALS_theta_set(false),
+            m_last_theta_set(false)
+        {};
+      //}
 
-    theta_t fit(const us_t& us, const Ps_t& Ps)
-    {
-      const int n = us.cols();
-      const zs_t zs = us.block(0, 0, lr, n);
-
-      // Find initial conditions through ALS
-      const auto [theta0, zc0] = fit_ALS(zs);
-      nu_t nu = theta0.template block<lr, 1>(0, 0);
-      z_t last_zc = zc0;
+      /* fit() method //{ */
+      /*!
+        * \brief Fit the defined model to the provided data.
+        *
+        * The RHEIV iterative optimization algorithm will be applied to estimate optimal parameters of the model based on the provided data.
+        *
+        * \param xs the data points \f$ \mathbf{x}_i \f$.
+        * \param Ps the corresponding covariance matrices\f$ \mathbf{P}_i \f$ .
+        *
+        * \returns  estimate of the parameter vector \f$ \mathbf{\theta} \f$.
+        *
+        * \warning  Note that length of \p xs and \p Ps must be the same!
+        *
+        */
+        theta_t fit(const xs_t& xs, const Ps_t& Ps)
+        {
+          assert(m_initialized);
+          assert((size_t)xs.cols() == Ps.size());
       
-      for (unsigned it = 0; it < m_max_its; it++)
-      {
-          std::cout << "-------------------------------------" << std::endl;
-          std::cout << "iteration: " << it << std::endl;
-          const nu_t prev_nu = nu;
-          std::cout << "pre-nu:" << std::endl << nu << std::endl;
-          const auto [M, N, zc] = calc_MN(nu, zs, Ps, m_dzdx);
-          std::cout << "M:" << std::endl << M << std::endl;
-          std::cout << "N:" << std::endl << N << std::endl;
-          last_zc = zc;
-          nu = calc_min_eigvec(M, N);
-          std::cout << "post-nu:" << std::endl << nu << std::endl;
-          if ((prev_nu - nu).norm() < m_min_dnu)
-              break;
-      }
-      const theta_t theta = calc_theta(nu, last_zc);
-      return theta;
-    }
+          const zs_t zs = m_f_z(xs);
+      
+          // Find initial conditions through ALS
+          m_ALS_theta = fit_ALS(zs);
+          m_ALS_theta_set = true;
+          eta_t eta = m_ALS_theta.template block<lr, 1>(0, 0);
+      
+          for (unsigned it = 0; it < m_max_its; it++)
+          {
+              const theta_t prev_theta = m_last_theta;
+              const auto [M, N, zc] = calc_MN(eta, zs, Ps, m_dzdx);
+              eta = calc_min_eigvec(M, N);
+              m_last_theta = calc_theta(eta, zc);
+              m_last_theta_set = true;
+              if ((prev_theta - m_last_theta).norm() < m_min_dtheta)
+                  break;
+          }
+          return m_last_theta;
+        }
+      //}
 
-  protected:
+      /* last_valid_estimate() method //{ */
+      /*!
+        * \brief Returns the last valid estimate of \f$ \mathbf{\theta} \f$.
+        *
+        * You can use this method in case of an exception to retreive the last valid estimate of the parameter vector
+        * before the crash, which is sometimes useful.
+        *
+        * \returns  the last valid estimate of the parameter vector \f$ \mathbf{\theta} \f$.
+        *
+        * \warning  The fit() method must be called prior to attempting to get the last valid estimate!
+        *
+        */
+        theta_t last_valid_estimate() const
+        {
+          assert(m_last_theta_set);
+          return m_last_theta;
+        }
+      //}
+
+      /* ALS_estimate() method //{ */
+      /*!
+        * \brief Returns the Algebraic Least Squares estimate of \f$ \mathbf{\theta} \f$.
+        *
+        * You can use this method in case of an exception to retreive the Algebraic Least Squares estimate of the parameter vector,
+        * which is more stable than the RHEIV algorithm, and can be used as a more rough estimate of the parameters.
+        *
+        * \returns  the ALS estimate of the parameter vector \f$ \mathbf{\theta} \f$.
+        *
+        * \warning  The fit() method must be called prior to attempting to get the last ALS estimate!
+        *
+        */
+        theta_t ALS_estimate() const
+        {
+          assert(m_ALS_theta_set);
+          return m_ALS_theta;
+        }
+      //}
+
+  private:
+    bool m_initialized;
+
+  private:
+    f_z_t m_f_z;
     dzdx_t m_dzdx;
-    double m_min_dnu;
+    double m_min_dtheta;
     unsigned m_max_its;
 
-  protected:
-    std::tuple<M_t, N_t, z_t> calc_MN(const nu_t& nu, const zs_t& zs, const Ps_t& Ps, const dzdx_t& dzdx)
+  private:
+    bool m_ALS_theta_set;
+    theta_t m_ALS_theta;
+    bool m_last_theta_set;
+    theta_t m_last_theta;
+
+  private:
+    std::tuple<M_t, N_t, z_t> calc_MN(const eta_t& eta, const zs_t& zs, const Ps_t& Ps, const dzdx_t& dzdx)
     {
       const int n = zs.cols();
     
       const Bs_t Bs = calc_Bs(Ps, dzdx);
-      const betas_t betas = calc_betas(nu, Bs);
+      const betas_t betas = calc_betas(eta, Bs);
       const auto [zrs, zc] = reduce_zs(zs, betas);
       
       M_t M = M_t::Zero();
@@ -121,7 +243,7 @@ namespace mrs_lib
           const B_t& B = Bs.at(it);
           const A_t A = calc_A(zr);
           M += A/beta;
-          N += (nu.transpose()*A*nu)*B/(beta*beta);
+          N += (eta.transpose()*A*eta)*B/(beta*beta);
       }
       return {M, N, zc};
     }
@@ -140,7 +262,7 @@ namespace mrs_lib
       return Bs;
     }
 
-    betas_t calc_betas(const nu_t& nu, const Bs_t& Bs)
+    betas_t calc_betas(const eta_t& eta, const Bs_t& Bs)
     {
       const int n = Bs.size();
 
@@ -148,7 +270,7 @@ namespace mrs_lib
       for (int it = 0; it < n; it++)
       {
           const B_t& B = Bs.at(it);
-          const double beta = 1/(nu.transpose()*B*nu);
+          const double beta = 1/(eta.transpose()*B*eta);
           betas(it) = beta;
       }
       return betas;
@@ -159,33 +281,30 @@ namespace mrs_lib
       return z*z.transpose();
     }
 
-    theta_t calc_theta(const nu_t& nu, const z_t& zc)
+    theta_t calc_theta(const eta_t& eta, const z_t& zc)
     {
-      const double alpha = -zc.transpose()*nu;
-      const theta_t theta( (theta_t() << nu, alpha).finished().normalized());
+      const double alpha = -zc.transpose()*eta;
+      const theta_t theta( (theta_t() << eta, alpha).finished().normalized());
       return theta;
     }
 
-    nu_t calc_min_eigvec(const M_t& M, const N_t& N)
+    eta_t calc_min_eigvec(const M_t& M, const N_t& N)
     {
       const Eigen::GeneralizedSelfAdjointEigenSolver<M_t> es(M, N);
       if (es.info() != Eigen::Success)
-        // TODO: throw an exception
-        std::cerr << "GeneralizedSelfAdjointEigenSolver failed: " << es.info() << std::endl; 
+        throw eigenvector_exception();
       // eigenvalues are sorted in increasing order when using the GeneralizedSelfAdjointEigenSolver as per Eigen documentation
-      const nu_t evec = es.eigenvectors().col(0).normalized(); // altough the Eigen documentation states that this vector should already be normalized, it isn't!!
-      /* std::cout << "GeneralizedSelfAdjointEigenSolver eigenvec norm: " << evec.norm() << std::endl; */
+      const eta_t evec = es.eigenvectors().col(0).normalized(); // altough the Eigen documentation states that this vector should already be normalized, it isn't!!
       return evec;
     }
 
-    nu_t calc_min_eigvec(const M_t& M)
+    eta_t calc_min_eigvec(const M_t& M)
     {
       const Eigen::SelfAdjointEigenSolver<M_t> es(M);
       if (es.info() != Eigen::Success)
-        std::cerr << "SelfAdjointEigenSolver failed: " << es.info() << std::endl; 
+        throw eigenvector_exception();
       // eigenvalues are sorted in increasing order when using the SelfAdjointEigenSolver as per Eigen documentation
-      const nu_t evec = es.eigenvectors().col(0).normalized(); // altough the Eigen documentation states that this vector should already be normalized, it isn't!!
-      std::cout << "SelfAdjointEigenSolver eigenvec norm: " << evec.norm() << std::endl;
+      const eta_t evec = es.eigenvectors().col(0).normalized(); // altough the Eigen documentation states that this vector should already be normalized, it isn't!!
       return evec;
     }
 
@@ -218,7 +337,7 @@ namespace mrs_lib
       return {zrs, zc};
     }
 
-    std::pair<theta_t, z_t> fit_ALS(const zs_t& zs)
+    theta_t fit_ALS(const zs_t& zs)
     {
       const auto [zrs, zc] = reduce_zs(zs);
       M_t M = M_t::Zero();
@@ -228,9 +347,9 @@ namespace mrs_lib
         const A_t A = calc_A(z);
         M += A;
       }
-      const nu_t nu = calc_min_eigvec(M);
-      theta_t theta = calc_theta(nu, zc);
-      return {theta, zc};
+      const eta_t eta = calc_min_eigvec(M);
+      const theta_t theta = calc_theta(eta, zc);
+      return theta;
     }
 
   };
--- src/rheiv/example.cpp
+++ src/rheiv/example.cpp
@@ -1,9 +1,13 @@
 // clang: MatousFormat
 
 /**  \file
-     \brief Example file for the LKF implementation
+     \brief Example file for the RHEIV implementation
      \author Matouš Vrba - vrbamato@fel.cvut.cz
-     This example may be run after building *mrs_lib* by executing 'rosrun mrs_lib lkf_example'.
+
+     This example may be run after building *mrs_lib* by executing 'rosrun mrs_lib rheiv_example'.
+
+     A plane surface model in the cartesian form \f$ a x + b y + c z + d = 0 \f$ will be used in this example.
+     The parameters \f$ a \f$, \f$ b \f$, \f$ c \f$ and \f$ d \f$ are elements of the parameter vector \f$ \mathbf{ \theta } \f$.
  */
 
 // Include the LKF header
@@ -18,24 +22,31 @@ namespace mrs_lib
 
   using rheiv_t = RHEIV<n_states, n_params>;
   using theta_t = rheiv_t::theta_t;
-  using us_t = rheiv_t::us_t;
+  using xs_t = rheiv_t::xs_t;
+  using zs_t = rheiv_t::zs_t;
   using P_t = rheiv_t::P_t;
   using Ps_t = rheiv_t::Ps_t;
+  using f_z_t = rheiv_t::f_z_t;
   using dzdx_t = rheiv_t::dzdx_t;
 }
 
+/* For the plane surface model, there is no need to transform the data. */
+mrs_lib::zs_t f_z(const mrs_lib::xs_t & xs)
+{
+  return xs;
+}
 
 int main()
 {
+  /* For the plane surface model, the Jacobian is just an identitiy matrix. */
   const mrs_lib::dzdx_t dzdx = mrs_lib::dzdx_t::Identity();
 
   const int n_pts = 4;
-  mrs_lib::us_t us(4, n_pts);
-  us <<
+  mrs_lib::xs_t xs(3, n_pts);
+  xs <<
     0, 1, 0, 10,
     0, 0, 1, 0,
-    0, 0, 0, 1,
-    1, 1, 1, 1;
+    0, 0, 0, 0.01;  // if you change the last value to 0, the iterative optimization will fail because the matrices M and N will be degenerate
   const mrs_lib::P_t P = mrs_lib::P_t::Identity();
   mrs_lib::Ps_t Ps;
   Ps.reserve(n_pts);
@@ -43,9 +54,18 @@ int main()
     Ps.push_back(P);
 
   std::cout << "initializing object:" << std::endl;
-  mrs_lib::rheiv_t rheiv(dzdx, 1e-15, 10);
+  mrs_lib::rheiv_t rheiv(f_z, dzdx, 1e-15, 10);
   std::cout << "running fit" << std::endl;
-  mrs_lib::theta_t theta = rheiv.fit(us, Ps);
+  mrs_lib::theta_t theta;
+  try
+  {
+    theta = rheiv.fit(xs, Ps);
+  } catch (const mrs_lib::rheiv_t::eigenvector_exception& ex)
+  {
+    std::cerr << "Iteration failed with message '" << ex.what() << "'." << std::endl;
+    std::cout << "using last valid theta" << std::endl;
+    theta = rheiv.last_valid_estimate();
+  }
   std::cout << "finished:" << std::endl << theta << std::endl;
 }
 
