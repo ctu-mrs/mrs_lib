4bc65aa920a20d14959615e06ab13209566d5ba0
Matouš Vrba <vrbamato@MyslPlacka>
2019-09-24 11:12:55 +0200

    added some rudimentary tests for geometry_utils

--- src/geometry_utils/geometry_utils_tests.cpp
+++ src/geometry_utils/geometry_utils_tests.cpp
@@ -9,9 +9,14 @@ int main()
 {
 
   Eigen::Vector3d vec1(1, 0, 0);
-  Eigen::Vector3d vec2(-1, 0, 0);
-  const auto rot = rotation_between(vec1, vec2);
-  cout << rot << std::endl;
+  Eigen::Vector3d vec2(-10, 0, 0);
+  Eigen::Vector3d vec3(1, 1, 0);
+  const auto rot_iden = rotation_between(vec1, vec1);
+  const auto rot_180 = rotation_between(vec1, vec2);
+  const auto rot_45 = rotation_between(vec1, vec3);
+  cout << "Should be identity:" << std::endl << rot_iden << std::endl;
+  cout << "Should be 180 deg rotation:" << std::endl << rot_180 << std::endl;
+  cout << "Should be 45 deg rotation:" << std::endl << rot_45 << std::endl;
 
   return 0;
 }
