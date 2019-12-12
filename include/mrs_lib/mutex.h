b90e340aeba6fae50ea09eac340a458adae1a686
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-12-12 13:28:14 +0100

    added set_mutexed()

--- include/mrs_lib/mutex.h
+++ include/mrs_lib/mutex.h
@@ -2,6 +2,7 @@
 #define MUTEX_H
 
 #include <mutex>
+#include <tuple>
 
 namespace mrs_lib
 {
@@ -22,6 +23,48 @@ T get_mutexed(std::mutex& mut, T& arg) {
   std::scoped_lock lock(mut);
 
   return arg;
+}
+
+template <class T, class... Args>
+auto set_mutexed_even(T& first, Args&... args);
+template <class T>
+auto set_mutexed_odd(T& first);
+template <class T, class... Args>
+auto set_mutexed_odd(T& first, Args&... args);
+
+template <class T>
+auto set_mutexed_odd(T& first) {
+
+  return first;
+}
+
+template <class T, class... Args>
+auto set_mutexed_odd(T& first, Args&... args) {
+
+  set_mutexed_even(args...);
+  return first;
+}
+
+template <class T, class... Args>
+auto set_mutexed_even(T& first, Args&... args) {
+
+  first = set_mutexed_odd(args...);
+}
+
+template <class... Args>
+void set_mutexed(std::mutex& mut, Args&... args) {
+
+  std::scoped_lock lock(mut);
+
+  set_mutexed_even(args...);
+}
+
+template <class... Args>
+void set_mutexed(std::mutex& mut, std::tuple<Args&...> to, std::tuple<Args&...> from) {
+
+  std::scoped_lock lock(mut);
+
+  to = from;
 }
 
 }  // namespace mrs_lib
