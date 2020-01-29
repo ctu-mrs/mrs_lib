7235de11a8c1434e0cf31961ba7b389be7b150d3
Viktor Walter <viktor_walter@outlook.com>
2020-01-29 08:51:33 +0400

    Adding functions for easily printing contents of vectors (also works with boost::array used in ros messages)

--- include/mrs_lib/Utils.h
+++ include/mrs_lib/Utils.h
@@ -3,9 +3,33 @@
 #define UTILS_H
 
 #include <iterator>
+#include <vector>
+#include <iostream>
+#include <boost/array.hpp>
 
 namespace mrs_lib
 {
+  //This could probaby be polymorphised more elegantly by somehow taking general input with iterator, but I can't be bothered to research this
+  template<typename T, typename A>
+  std::string vectorToString(std::vector<T,A> const& input, const std::string& delimiter){
+    bool first = true;
+    std::ostringstream output;
+    for (auto e : input){
+      if (!first)
+      output << delimiter;
+      output << e;
+        first = false;
+    }
+    return output.str();
+  }
+  template<typename T, typename A>
+  std::string vectorToString(const std::vector<T,A>& input, const char* delimiter){
+    return vectorToString(input, std::string(delimiter));
+  }
+  template< class T, std::size_t N >
+  std::string vectorToString(const boost::array<T,N>& input, const char* delimiter){
+    return vectorToString(std::vector<T>(input.begin(), input.end()), std::string(delimiter));
+  }
 
   /* remove_cons() function //{ */
   // TODO what does this do?
