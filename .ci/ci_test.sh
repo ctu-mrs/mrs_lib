cb80e41f5b211370da87a6f3a70176e35884379a
klaxalk <klaxalk@gmail.com>
2020-10-15 01:20:17 +0200

    updated ci

--- .ci/ci_test.sh
+++ .ci/ci_test.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
 distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
@@ -12,8 +15,11 @@ cd ~/catkin_ws
 source /opt/ros/$ROS_DISTRO/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
+catkin build mrs_lib
 catkin build mrs_lib --catkin-make-args tests
-rostest mrs_lib geometry_utils_test.launch -t --results-filename=mrs_lib.test --results-base-dir=/tmp
-catkin_test_results /tmp
+TEST_RESULT_PATH=$(realpath /tmp/$RANDOM)
+mkdir -p $TEST_RESULT_PATH
+rostest mrs_lib geometry_utils_test.launch -t --results-filename=mrs_lib.test --results-base-dir="$TEST_RESULT_PATH"
+catkin_test_results "$TEST_RESULT_PATH"
 
 echo "Test ended"
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+catkin build mrs_lib
 catkin build mrs_lib --catkin-make-args tests
-rostest mrs_lib geometry_utils_test.launch -t --results-filename=mrs_lib.test --results-base-dir=/tmp/mrs_lib
-catkin_test_results /tmp/mrs_lib
+TEST_RESULT_PATH=$(realpath /tmp/$RANDOM)
+mkdir -p $TEST_RESULT_PATH
+rostest mrs_lib geometry_utils_test.launch -t --results-filename=mrs_lib.test --results-base-dir="$TEST_RESULT_PATH"
+catkin_test_results "$TEST_RESULT_PATH"
+
+echo ""
+echo $?
