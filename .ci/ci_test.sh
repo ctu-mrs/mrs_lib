b2240b548f131a49ba61e5b46077e1a82971833b
klaxalk <klaxalk@gmail.com>
2020-10-15 08:09:01 +0200

    updated ci test

--- .ci/ci_test.sh
+++ .ci/ci_test.sh
@@ -15,11 +15,11 @@ cd ~/catkin_ws
 source /opt/ros/$ROS_DISTRO/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
-catkin build mrs_lib
+catkin build mrs_lib # it has to be fully build normally before building with --catkin-make-args tests
 catkin build mrs_lib --catkin-make-args tests
 TEST_RESULT_PATH=$(realpath /tmp/$RANDOM)
 mkdir -p $TEST_RESULT_PATH
-rostest mrs_lib geometry_utils_test.launch -t --results-filename=mrs_lib.test --results-base-dir="$TEST_RESULT_PATH"
+rostest mrs_lib geometry_utils_test.launch --results-filename=mrs_lib.test --results-base-dir="$TEST_RESULT_PATH"
 catkin_test_results "$TEST_RESULT_PATH"
 
 echo "Test ended"
