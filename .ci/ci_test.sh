c5a7e9187326b42083c29d31819aa55f7a046e3b
klaxalk <klaxalk@gmail.com>
2020-10-15 09:21:07 +0200

    fixing test launch file

--- .ci/ci_test.sh
+++ .ci/ci_test.sh
@@ -15,11 +15,24 @@ cd ~/catkin_ws
 source /opt/ros/$ROS_DISTRO/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
-catkin build mrs_lib # it has to be fully build normally before building with --catkin-make-args tests
-catkin build mrs_lib --catkin-make-args tests
+PACKAGE="mrs_lib"
+VERBOSE=0
+
+[ "$VERBOSE" = "0" ] && TEXT_OUTPUT=""
+[ "$VERBOSE" = "1" ] && TEXT_OUTPUT="-t"
+
+# build the package
+catkin build $PACKAGE # it has to be fully build normally before building with --catkin-make-args tests
+catkin build $PACKAGE --catkin-make-args tests
+
+# folder for test results
 TEST_RESULT_PATH=$(realpath /tmp/$RANDOM)
 mkdir -p $TEST_RESULT_PATH
-rostest mrs_lib geometry_utils_test.launch --results-filename=mrs_lib.test --results-base-dir="$TEST_RESULT_PATH"
+
+# run the test
+rostest $PACKAGE mrs_lib.test $TEXT_OUTPUT --results-filename=$PACKAGE.test --results-base-dir="$TEST_RESULT_PATH"
+
+# evaluate the test results
 catkin_test_results "$TEST_RESULT_PATH"
 
 echo "Test ended"
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -20,7 +20,7 @@ TEST_RESULT_PATH=$(realpath /tmp/$RANDOM)
 mkdir -p $TEST_RESULT_PATH
 
 # run the test
-rostest $PACKAGE geometry_utils.test $TEXT_OUTPUT --results-filename=$PACKAGE.test --results-base-dir="$TEST_RESULT_PATH"
+rostest $PACKAGE mrs_lib.test $TEXT_OUTPUT --results-filename=$PACKAGE.test --results-base-dir="$TEST_RESULT_PATH"
 
 # evaluate the test results
 catkin_test_results "$TEST_RESULT_PATH"
