be5372e06b5cb7e8abf6eb05e528294ae24f8085
klaxalk <klaxalk@gmail.com>
2020-10-15 08:41:44 +0200

    enclosed tests into subfolder

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,7 +5,7 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
-catkin build mrs_lib
+catkin build mrs_lib # it has to be fully build normally before building with --catkin-make-args tests
 catkin build mrs_lib --catkin-make-args tests
 TEST_RESULT_PATH=$(realpath /tmp/$RANDOM)
 mkdir -p $TEST_RESULT_PATH
@@ -13,4 +13,4 @@ rostest mrs_lib geometry_utils_test.launch -t --results-filename=mrs_lib.test --
 catkin_test_results "$TEST_RESULT_PATH"
 
 echo ""
-echo $?
+echo "Success? $?"
