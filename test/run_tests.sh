799ec3f976d0267e8b88df8ab2a16f0a6feb3434
klaxalk <klaxalk@gmail.com>
2020-10-15 08:47:21 +0200

    updated tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,11 +5,16 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
+VERBOSE=0
+
+[ "$VERBOSE" = "0" ] && TEXT_OUTPUT=""
+[ "$VERBOSE" = "1" ] && TEXT_OUTPUT="-t"
+
 catkin build mrs_lib # it has to be fully build normally before building with --catkin-make-args tests
 catkin build mrs_lib --catkin-make-args tests
 TEST_RESULT_PATH=$(realpath /tmp/$RANDOM)
 mkdir -p $TEST_RESULT_PATH
-rostest mrs_lib geometry_utils_test.launch -t --results-filename=mrs_lib.test --results-base-dir="$TEST_RESULT_PATH"
+rostest mrs_lib geometry_utils.test $TEXT_OUTPUT --results-filename=mrs_lib.test --results-base-dir="$TEST_RESULT_PATH"
 catkin_test_results "$TEST_RESULT_PATH"
 
 echo ""
