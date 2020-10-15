ff2d43d16defff979b302481fecbb83848b38cb4
klaxalk <klaxalk@gmail.com>
2020-10-15 16:58:59 +0200

    added param loader test

--- test/param_loader/test.cpp
+++ test/param_loader/test.cpp
@@ -1,4 +1,12 @@
 #include <mrs_lib/param_loader.h>
+#include <cmath>
+#include <iostream>
+
+#include <gtest/gtest.h>
+#include <log4cxx/logger.h>
+
+using namespace mrs_lib;
+using namespace std;
 
 std::vector<double> gemerate_nd_matrix(int rows, const std::vector<int>& cols)
 {
@@ -14,20 +22,16 @@ std::vector<double> gemerate_nd_matrix(int rows, const std::vector<int>& cols)
   return ret;
 }
 
-int main(int argc, char **argv)
-{
+/* TEST(TESTSuite, main_test) //{ */
+
+TEST(TESTSuite, main_test) {
+
+  int result = 1;
+
   // Set up ROS.
-  ros::init(argc, argv, "param_loader_tests");
   ros::NodeHandle nh = ros::NodeHandle("~");
 
-  /* int rows = 4; */
-  /* std::vector<int> cols = {3, 2, 1}; */
-
-  /* ROS_INFO("[%s]: pushing testing parameters to the rosparam server", ros::this_node::getName().c_str()); */
-  /* std::vector<double> param_nd_matrix = gemerate_nd_matrix(rows, cols); */
-  /* nh.setParam("test_param_nd_matrix/rows", rows); */
-  /* nh.setParam("test_param_nd_matrix/cols", cols); */
-  /* nh.setParam("test_param_nd_matrix/data", param_nd_matrix); */
+  ros::Time::waitForValid();
 
   ROS_INFO("[%s]: initializing ParamLoader", ros::this_node::getName().c_str());
   mrs_lib::ParamLoader pl(nh);
@@ -50,13 +54,33 @@ int main(int argc, char **argv)
   } else
   {
     ROS_ERROR("[%s]: parameter loading failure", ros::this_node::getName().c_str());
-    return 1;
+    result *= 0;
   }
 
   std::cout << "Loading empty matrix" << std::endl;
-  nh.setParam("test_param_matrix", std::vector<double>());
   Eigen::Matrix<double, 0, 0> mat = pl.loadMatrixStatic2<0, 0>("test_param_matrix");
   std::cout << "Empty matrix:" << std::endl << mat;
 
-  return 0;
+  if (fabs(loaded_nd_matrix.at(0)(0, 0) - (-5)) > 1e-6) {
+    ROS_INFO("[%s]: (0, 0) of the fist matrix failed", ros::this_node::getName().c_str());
+    result *= 0;
+  }
+
+  if (fabs(loaded_nd_matrix.at(1)(2, 0) - (4)) > 1e-6) {
+    ROS_INFO("[%s]: (2, 0) of the second matrix failed", ros::this_node::getName().c_str());
+    result *= 0;
+  }
+
+  EXPECT_TRUE(result);
+}
+
+//}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+
+  ros::init(argc, argv, "param_loader_tests");
+
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
 }
