c36ee66f26d9dc2c83530df93619d6f1fa34faea
Tomáš Báča <klaxalk@gmail.com>
2020-10-16 19:39:27 +0200

    Adding more rostests (#16)
    * added attitude converter tests
    
    * added utils test
    
    * added transformer test
    
    * added param loader test
    
    * added subscribe handler test

---
+++ test/utils/test.cpp
@@ -0,0 +1,44 @@
+#include <mrs_lib/utils.h>
+#include <cmath>
+#include <iostream>
+#include <boost/array.hpp>
+
+#include <gtest/gtest.h>
+#include <log4cxx/logger.h>
+
+using namespace mrs_lib;
+using namespace std;
+
+/* TEST(TESTSuite, container_to_string) //{ */
+
+TEST(TESTSuite, container_to_string) {
+
+  int result = 1;
+
+  std::vector<int> vec = {1, 2, 666};
+  std::string      str = mrs_lib::containerToString(std::begin(vec) + 1, std::end(vec), ";");
+  std::cout << str << std::endl;
+
+  if (str != "2;666") {
+    result *= 0;
+  }
+
+  boost::array<int, 3> arr  = {6, 6, 6};
+  std::string          str2 = mrs_lib::containerToString(arr, ", ");
+
+  if (str2 != "6, 6, 6") {
+    result *= 0;
+  }
+  std::cout << str2 << std::endl;
+
+  EXPECT_TRUE(result);
+}
+
+//}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
