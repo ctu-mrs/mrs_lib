60ed092a642108a43b015c4ca091ec4a5a3010d1
Matouš Vrba <vrbamato@fel.cvut.cz>
2020-10-27 14:34:52 +0100

    cyclical values implemented and documented

--- include/mrs_lib/geometry_utils.h
+++ include/mrs_lib/geometry_utils.h
@@ -37,27 +37,92 @@ namespace mrs_lib
       return ret;
     }
 
+    /**
+    * \brief Implementation of the a general cyclic value (such as angles in radians/degrees etc).
+    *
+    * This class implements a periodical value with a period of \f$ r \in {\rm I\!R} \f$ so that a general number \f$ v \in {\rm I\!R} \f$ represents the same value as \f$ v + kr,~k \in {\rm I\!N} \f$.
+    * For the purposes of calculations in this class, \f$ v \f$ is confined to a half-open interval \f$ v \in [~m,~s~[ \f$, where \f$ m \f$ is the minimum of this interval and \f$ s \f$ is its supremum, and \f$ s = m + r \f$ holds.
+    * This approach enables representing \f$ v \f$ in different intervals on the real numbers axis; eg. angle in radians may be represented within the interval \f$ v \in [~-\pi,~\pi~[ \f$ or \f$ v \in [~0,~2\pi~[ \f$, according to the needs of the specific application.
+    * The period \f$ r \f$ is called \p range for the purposes of this class, as it represents range of the interval of valid ("wrapped") values of \f$ v \f$.
+    *
+    * This class may be used as an object or its static methods may be used on regular floating-point types, avoiding any object-related overheads (see example).
+    * Specializations for the most common cyclic values are provided and a new specialization may be easily created simply by inheriting from this class and specifying a different minimum and supremum values.
+    *
+    * \parblock
+    * \note For a better intuitive understanding of the used functions, the term **walk** is sometimes used in the function explanations.
+    * You can imagine walking along the circle from one angle to another (represented by the circular quantities).
+    * The walk may be the shortest - then you're walking in such a manner that you reach the other point in the least of steps.
+    * The walk may also be oriented - then you're walking in a specific direction (ie. according to the increasing/decreasing angle).
+    * \endparblock
+    *
+    * \parblock
+    * \note The terms **circular quantity** and **value** are used in the function explanations.
+    * A circular quantity is eg. an angle and the same quantity may be represented using different values: the same angle is represented by the values of 100 degrees and 460 degrees.
+    * \endparblock
+    *
+    * \tparam flt floating data type to be used by this class.
+    *
+    */
     template <typename flt>
     struct cyclic
     {
+  /*!
+    * \brief Default constructor.
+    *
+    * Sets the value to the minimum.
+    */
+      cyclic() : val(minimum) {};
+  /*!
+    * \brief Constructor overload.
+    *
+    * \param val initialization value (will be wrapped).
+    */
       cyclic(const flt val) : val(wrap(val)) {};
-      cyclic& operator=(const flt nval) {val = wrap(nval);};
-      cyclic& operator=(const cyclic& other) {val = other.val;};
+  /*!
+    * \brief Assignment operator.
+    *
+    * \param nval value to be assigned (will be wrapped).
+    */
+      cyclic& operator=(const flt nval) {val = wrap(nval); return *this;};
+  /*!
+    * \brief Assignment operator.
+    *
+    * \param other value to be assigned.
+    */
+      cyclic& operator=(const cyclic& other) {val = other.val; return *this;};
+  /*!
+    * \brief Cast operator.
+    *
+    * \returns the current value.
+    */
+      operator flt() {return val;};
 
-      const flt val;
-
-      static constexpr flt minimum = 0;
-      static constexpr flt supremum = 2*M_PI;
-      static constexpr flt range = supremum - minimum;
-      static constexpr flt half_range = range/flt(2);
+      static constexpr flt minimum = 0;                 /*!< \brief Minimum of the valid interval of wrapped values \f$ m \f$ */
+      static constexpr flt supremum = 2*M_PI;           /*!< \brief Supremum of the valid interval of wrapped values \f$ s \f$ */
+      static constexpr flt range = supremum - minimum;  /*!< \brief Range of the valid interval of wrapped values \f$ r \f$ (also the period of the cyclic quantity). */
+      static constexpr flt half_range = range/flt(2);   /*!< \brief Half of the range of the valid interval of wrapped values \f$ r/2 \f$ (used for some calculations). */
 
       static_assert((supremum > minimum), "cyclic value: Range not valid");   
 
-      static flt inRange(const flt val)
+  /*!
+    * \brief Checks if \p val is within the valid interval of wrapped values.
+    *
+    * \param val the value to be checked.
+    * \returns   true if \p val is within the valid interval of wrapped values.
+    */
+      static bool inRange(const flt val)
       {
         return val >= minimum && val < supremum;
       }
 
+  /*!
+    * \brief Returns \p val, converted to the valid interval of values.
+    *
+    * The wrapped value represents the same quantity as the parameter (ie. \f$ v' = v + kr \f$, where \f$ v \f$ is the parameter and \f$ v' \f$ is the returned value).
+    *
+    * \param val the value to be wrapped.
+    * \returns   \p val wrapped to the valid interval of values.
+    */
       static flt wrap(const flt val)
       {
         // these few ifs should cover most cases, improving speed and precision
@@ -80,11 +145,40 @@ namespace mrs_lib
         return wrapped;
       }
 
+  /*!
+    * \brief Returns value of the parameter \p to modified so that there is no "jump" between \p from and \t to.
+    *
+    * The circular difference between the two input quantities is preserved, but the returned value is modified if necessary so that the linear distance between the values is the smallest.
+    * This is useful whenever you need to preserve linear continuity of consecutive values, eg. when commanding a multi-rotational servo motor or when using a simple linear Kalman filter to estimate circular quantities.
+    *
+    * An example of inputs and outputs if \f$ m = 0,~s=360 \f$:
+    *  \p from  | \p to | \p return
+    *  -------- | ------| ---------
+    *     10    |   20  |   20
+    *     350   |   20  |   380
+    *     350   |   0   |   360
+    *     10    |   200 |  -160
+    *
+    * \param from   the previous value from which the unwrapped value of \p to should have the same circular difference and minimal linear distance.
+    * \param to     the value to be unwrapped.
+    * \returns      the unwrapped value of \p to.
+    *
+    * \warning Note that the returned value may be outside the valid interval of wrapped values, specified by the \p minimum and \p supremum parameters of this class.
+    */
       static flt unwrap(const flt from, const flt to)
       {
         return from + diff(to, from);
       }
 
+  /*!
+    * \brief Returns length of the shortest walk in the positive direction from the first parameter to the second one.
+    *
+    * \param from   the positive walk starts at this value.
+    * \param to     the positive walk ends at this value.
+    * \returns      length of the shortest positive walk from the first parameter to the second one.
+    *
+    * \note The returned value is not necessarily the shortest distance between the two circular quantities (see the dist() function for that).
+    */
       static flt pdist(const flt from, const flt to)
       {
         const flt wfrom = wrap(from);
@@ -101,6 +195,15 @@ namespace mrs_lib
         return dist;
       }
 
+  /*!
+    * \brief Returns the difference between the two circular values.
+    *
+    * The difference may also be interpreted as length of the shortest walk between the two values, with a sign according to the direction of the shortest walk.
+    *
+    * \param minuend      the \p subtrahend will be subtracted from this value.
+    * \param subtrahend   this value will be subtracted from the \p minuend.
+    * \returns            the difference of the two circular quantities.
+    */
       static flt diff(const flt minuend, const flt subtrahend)
       {
         const flt wminuend = wrap(minuend);
@@ -113,7 +216,7 @@ namespace mrs_lib
         return d;
       }
 
-      static cyclic diff(const cyclic minuend, const cyclic subtrahend)
+      static flt diff(const cyclic minuend, const cyclic subtrahend)
       {
         const flt d = minuend.value - subtrahend.value;
         if (d < -half_range)
@@ -123,6 +226,17 @@ namespace mrs_lib
         return d;
       }
 
+  /*!
+    * \brief Returns the distane between the two circular values.
+    *
+    * The distance may also be interpreted as length of the shortest walk between the two values.
+    *
+    * \param from  the first circular quantity.
+    * \param to    the second circular quantity.
+    * \returns     distance of the two circular quantities.
+    *
+    * \note The order of the parameters doesn't matter.
+    */
       static flt dist(const flt from, const flt to)
       {
         return std::abs(diff(from, to));
@@ -133,24 +247,54 @@ namespace mrs_lib
         return std::abs(diff(from, to));
       }
 
-      static flt interp_unwrapped(const flt from, const flt to, const flt coeff)
+  /*!
+    * \brief Interpolation between two circular quantities without wrapping of the result.
+    *
+    * This function doesn't wrap the returned value.
+    *
+    * \param from  the first circular quantity.
+    * \param to    the second circular quantity.
+    * \param coeff the interpolation coefficient.
+    * \returns     interpolation of the two circular quantities using the coefficient.
+    *
+    * \warning Note that the returned value may be outside the valid interval of wrapped values, specified by the \p minimum and \p supremum parameters of this class.
+    */
+      static flt interpUnwrapped(const flt from, const flt to, const flt coeff)
       {
         const flt diff = diff(to, from);
         const flt intp = from + coeff*diff;
         return intp;
       }
 
-      static cyclic interp_unwrapped(const cyclic from, const cyclic to, const flt coeff)
+      static flt interpUnwrapped(const cyclic from, const cyclic to, const flt coeff)
       {
-        const cyclic diff = diff(to, from);
-        const cyclic intp = from + coeff*diff;
+        const flt diff = diff(to, from);
+        const flt intp = from + coeff*diff;
         return intp;
       }
 
+  /*!
+    * \brief Interpolation between two circular quantities without wrapping of the result.
+    *
+    * This function wraps the returned value so that it is in the interval of valid values.
+    *
+    * \param from  the first circular quantity.
+    * \param to    the second circular quantity.
+    * \param coeff the interpolation coefficient.
+    * \returns     interpolation of the two circular quantities using the coefficient, wrapped to the interval of valid values.
+    */
       static flt interp(const flt from, const flt to, const flt coeff)
       {
         return wrap(interp_unwrapped(from, to, coeff));
       }
+
+      static cyclic interp(const cyclic from, const cyclic to, const flt coeff)
+      {
+        return interp_unwrapped(from, to, coeff);
+      }
+
+    private:
+      flt val;
     };
 
     struct radians : public cyclic<double>
