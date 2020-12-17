c2a2680904969619bd47418d062dddd7aaf2aa92
Matouš Vrba <vrbamato@fel.cvut.cz>
2020-12-17 10:02:06 +0100

    added comparison with LKF to the example

--- src/repredictor/example.cpp
+++ src/repredictor/example.cpp
@@ -94,6 +94,7 @@ int main()
 {
   // Generate initial state, input and time
   const x_t x0 = x_t::Zero();
+  const P_t P0 = 10.0*P_t::Identity();
   const u_t u0 = u_t::Random();
   const ros::Time t0 = ros::Time(0);
   // H will observe the position
@@ -102,11 +103,14 @@ int main()
   const Q_t Q = 0.1*Q_t::Identity();
   // measurement noise is just identity
   const R_t R = 0.1*R_t::Identity();
+  
+  const int n_gts = 5e1;
+  const int n_meass = 3e1;
+
+  // Instantiate the LKF model
+  auto lkf = std::make_shared<lkf_t>(generateA, generateB, H);
 
   /* Prepare the ground-truth states //{ */
-  
-  const int n_gts = 5e2;
-  const int n_meass = 3e2;
   
   std::vector<x_t> gts(n_gts);
   std::vector<u_t> inputs(n_gts);
@@ -136,14 +140,28 @@ int main()
   
   ros::Time prev_stamp = t0;
   int prev_gt_it = 0;
-  std::vector<std::pair<z_t, ros::Time>> measurements(n_meass);
+  std::vector<std::tuple<ros::Time, z_t, x_t, u_t>> measurements(n_meass);
+  std::vector<statecov_t> lkf_scs(n_meass);
+  lkf_scs.front() = {x0, P0};
+  statecov_t lkf_sc;
+  ros::Time last_lkf_stamp = stamps.front();
   for (int it = 0; it < n_meass; it++)
   {
     // Generate a random dt
     const ros::Time meas_stamp = prev_stamp + ros::Duration(std::abs(d(gen))/n_meass*n_gts);
     // Move the prev_gt_it if necessary
     while (prev_gt_it < n_gts && stamps.at(prev_gt_it) < meas_stamp)
+    {
+      if (prev_gt_it > 0)
+      {
+        const auto u = inputs.at(prev_gt_it-1);
+        const auto stamp = stamps.at(prev_gt_it);
+        const double dt = (stamp-last_lkf_stamp).toSec();
+        lkf_sc = lkf->predict(lkf_sc, u, Q, dt);
+        last_lkf_stamp = stamp;
+      }
       prev_gt_it++;
+    }
     prev_gt_it--;
     // Get the closest last ground truths
     const auto x = gts.at(prev_gt_it);
@@ -156,19 +174,19 @@ int main()
     // Generate a new observation according to the model and noise parameters
     const z_t z = H*x_cur + normal_randmat(R);
     // Add the new measurement and its stamp to the vector
-    measurements.at(it) = {z, meas_stamp};
+    measurements.at(it) = {meas_stamp, z, x_cur, u};
+
+    lkf_sc = lkf->predict(lkf_sc, u, Q, (meas_stamp-last_lkf_stamp).toSec());
+    lkf_sc = lkf->correct(lkf_sc, z, R);
+    lkf_scs.at(it) = lkf_sc;
+
     // Update the helper variable
     prev_stamp = meas_stamp;
   }
 
   //}
   
-  // Instantiate the LKF model
-  auto lkf = std::make_shared<lkf_t>(generateA, generateB, H);
   // Instantiate the Repredictor itself
-  /* P_t P_tmp = P_t::Random(); */
-  /* const P_t P0 = 10.0*P_tmp*P_tmp.transpose(); */
-  const P_t P0 = 10.0*P_t::Identity();
   rep_t rep(x0, P0, u0, Q, t0, lkf, n_gts+n_meass);
 
   // Fill the buffer of the Repredictor
@@ -182,8 +200,8 @@ int main()
       // add the measurements randomly
       std::uniform_int_distribution<> ud(0, meas_remaining.size()-1);
       const int meas_idx = ud(gen);
-      const z_t z = meas_remaining.at(meas_idx).first;
-      const ros::Time stamp = meas_remaining.at(meas_idx).second;
+      const ros::Time stamp = std::get<0>(meas_remaining.at(meas_idx));
+      const z_t z = std::get<1>(meas_remaining.at(meas_idx));
       meas_remaining.erase(std::begin(meas_remaining) + meas_idx);
       rep.addMeasurement(z, R, stamp);
     }
@@ -198,27 +216,24 @@ int main()
   
   {
     std::ofstream ofs("inpt.csv");
-    ofs << "t,xgt,dxgt,xes,dxes,err" << std::endl;
-    std::vector<x_t> estimates(n_gts);
-    std::vector<P_t> covs(n_gts);
-    for (int it = 0; it < n_gts; it++)
+    ofs << "t,xgt,dxgt,xes,dxes,xkf,dxkf,err" << std::endl;
+    for (int it = 0; it < n_meass; it++)
     {
-      const auto stamp = stamps.at(it);
+      const auto [stamp, z, x_gt, u] = measurements.at(it);
       const auto [x, P] = rep.predictTo(stamp);
-      estimates.push_back(x);
-      covs.push_back(P);
-      const auto err = (gts.at(it)-x).norm();
-      std::cout << "xgt[" << it << "]: [" << gts.at(it).transpose() << "]^T\txes[" << it << "]: [" << x.transpose() << "]^T" << "\terr[" << it << "]:  " << err << std::endl;
-      ofs << stamp.toSec() << "," << gts.at(it).x() << "," << gts.at(it).y() << "," << x.x() << "," << x.y() << "," << err << std::endl;
+      const auto lkf_sc = lkf_scs.at(it);
+      const auto err = (x_gt-x).norm();
+      std::cout << "xgt[" << it << "]: [" << x_gt.transpose() << "]^T\txes[" << it << "]: [" << x.transpose() << "]^T" << "\terr[" << it << "]:  " << err << std::endl;
+      ofs << stamp.toSec() << "," << x_gt.x() << "," << x_gt.y() << "," << x.x() << "," << x.y() << "," << lkf_sc.x.x() << "," << lkf_sc.x.y() << "," << err << std::endl;
     }
   }
 
   {
     std::ofstream ofs("meas.csv");
-    ofs << "t,xmeas" << std::endl;
+    ofs << "t,xmeas,xgt,dxgt" << std::endl;
     for (const auto& el : measurements)
     {
-      ofs << el.second.toSec() << "," << el.first << std::endl;
+      ofs << std::get<0>(el).toSec() << "," << std::get<1>(el) << "," << std::get<2>(el).x() << "," << std::get<2>(el).y() << std::endl;
     }
   }
 
