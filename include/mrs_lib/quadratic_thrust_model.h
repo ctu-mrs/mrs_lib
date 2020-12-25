8c47239814674b1530d868940d13da91f5827fdd
Tomas Baca <klaxalk@gmail.com>
2020-12-25 20:57:25 +0100

    added quadratic thrust model

---
+++ include/mrs_lib/quadratic_thrust_model.h
@@ -0,0 +1,33 @@
+#ifndef QUADRATIC_THRUST_MODEL_H
+#define QUADRATIC_THRUST_MODEL_H
+
+#include <math.h>
+
+namespace mrs_lib
+{
+
+namespace quadratic_thrust_model
+{
+
+typedef struct
+{
+  double A;
+  double B;
+  int    n_motors;
+} MotorParams_t;
+
+double inline thrustToForce(const MotorParams_t motor_params, const double thrust) {
+
+  return motor_params.n_motors * pow((thrust - motor_params.B) / motor_params.A, 2);
+}
+
+double inline forceToThrust(const MotorParams_t motor_params, const double force) {
+
+  return sqrt(force / motor_params.n_motors) * motor_params.A + motor_params.B;
+}
+
+}  // namespace quadratic_thrust_model
+
+}  // namespace mrs_lib
+
+#endif  // QUADRATIC_THRUST_MODEL_H
