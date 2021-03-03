6196ca943a61087d001679a8024ee4d295602f9d
Daniel Hert <hertdani@fel.cvut.cz>
2021-03-03 13:36:22 +0100

    update

--- include/mrs_lib/iir_filter.h
+++ include/mrs_lib/iir_filter.h
@@ -14,29 +14,14 @@ class IirFilter {
 public:
   IirFilter(const std::vector<double>& a, const std::vector<double>& b);
 
-  double iterate(const double& input);
-  double SectCalcForm1(int k, double x);
+  double iterate(const double input);
 
 
 private:
-  std::vector<double> _a_;
-  std::vector<double> _b_;
-  size_t              _order_;
-  std::vector<double> input_buffer_;
-  std::vector<double> output_buffer_;
-
-  double RegX1 = 0.0;
-  double RegX2 = 0.0;
-  double RegY1 = 0.0;
-  double RegY2 = 0.0;
-
-  double a0 = 0.0;
-  double a1 = 0.0;
-  double a2 = 0.0;
-
-  double b0 = 0.0;
-  double b1 = 0.0;
-  double b2 = 0.0;
+  std::vector<double> a_;
+  std::vector<double> b_;
+  size_t              order_;
+  std::vector<double> buffer_;
 };
 
 }  // namespace mrs_lib
--- src/iir_filter/iir_filter.cpp
+++ src/iir_filter/iir_filter.cpp
@@ -9,8 +9,8 @@ namespace mrs_lib
 
 IirFilter::IirFilter(const std::vector<double>& a_in, const std::vector<double>& b_in) {
 
-  /* _a_ = a; */
-  /* _b_ = b; */
+  a_ = a_in;
+  b_ = b_in;
 
   ROS_INFO_STREAM("[" << ros::this_node::getName().c_str() << "]: IIR Filter initialized!");
   /* ROS_INFO_STREAM("[" << ros::this_node::getName().c_str() << "]: a: " << _a_[0] << " " << _a_[1] << " " << _a_[2]); */
@@ -23,46 +23,40 @@ IirFilter::IirFilter(const std::vector<double>& a_in, const std::vector<double>&
   /*   ROS_ERROR("[%s]: Parameters a and b needs to have the same non-zero length!!", ros::this_node::getName().c_str()); */
   /*   ros::shutdown(); */
   /* } */
-  ROS_INFO("pes]: ");
-  ROS_INFO("kot]: ");
-  a0 = a_in.at(0);
-  a1 = a_in.at(0);
-  a2 = a_in.at(0);
+  /* a0 = a_in.at(0); */
+  /* a1 = a_in.at(1); */
+  /* a2 = a_in.at(2); */
 
-  b0 = b_in.at(0);
-  b1 = b_in.at(0);
-  b2 = b_in.at(0);
+  /* b0 = b_in.at(0); */
+  /* b1 = b_in.at(1); */
+  /* b2 = b_in.at(2); */
 
-  _order_ = 3;
-  /* input_buffer_.resize(_order_ - 1, 0.0); */
-  /* output_buffer_.resize(_order_ - 1, 0.0); */
+
+  order_ = a_.size();
+  buffer_.resize(order_, 0.0);
 }
 
 //}
 
 /* iterate() //{ */
 
-double IirFilter::iterate(const double& input) {
+double IirFilter::iterate(const double input) {
 
-  double output;
-  output = SectCalcForm1(0, input);
-  output = SectCalcForm1(0, output);
+  double output = 0;
 
+  buffer_[0] = input;
 
+  for (size_t i = 1; i < order_; i++) {
+    buffer_[0] += (-a_[i]) * buffer_[i];
+    output += (b_[i]) * buffer_[i];
+  }
+
+  output += buffer_[0] * b_[0];
+
+  for (size_t i = order_ - 1; i > 0; i--) {
+    buffer_[i] = buffer_[i - 1];
+  }
   return output;
 }
 
-double IirFilter::SectCalcForm1(int k, double x) {
-  double y, CenterTap;
-
-  CenterTap = x * b0 + b1 * RegX1 + b2 * RegX2;
-  y         = a0 * CenterTap - a1 * RegY1 - a2 * RegY2;
-
-  RegX2 = RegX1;
-  RegX1 = x;
-  RegY2 = RegY1;
-  RegY1 = y;
-
-  return (y);
-}
 }  // namespace mrs_lib
