f1af541558ab54f083a7413bb25c7e223217ca69
Tomas Baca <klaxalk@gmail.com>
2022-03-01 08:28:51 +0200

    added publisher handler

---
+++ include/impl/publisher_handler.hpp
@@ -0,0 +1,122 @@
+#ifndef PUBLISHER_HANDLER_HPP
+#define PUBLISHER_HANDLER_HPP
+
+namespace mrs_lib
+{
+
+// --------------------------------------------------------------
+// |                    PublisherHandler_impl                   |
+// --------------------------------------------------------------
+
+/* PublisherHandler_impl(void) //{ */
+
+template <class TopicType>
+PublisherHandler_impl<TopicType>::PublisherHandler_impl(void) : publisher_initialized_(false) {
+}
+
+//}
+
+/* PublisherHandler_impl(ros::NodeHandle& nh, const std::string& address, const unsigned int &buffer_size, const bool &latch) //{ */
+
+template <class TopicType>
+PublisherHandler_impl<TopicType>::PublisherHandler_impl(ros::NodeHandle& nh, const std::string& address, const unsigned int &buffer_size, const bool &latch) {
+
+  {
+    std::scoped_lock lock(mutex_publisher_);
+
+    publisher_ = nh.advertise<TopicType>(address, buffer_size, latch);
+  }
+
+  _address_ = address;
+
+  publisher_initialized_ = true;
+}
+
+//}
+
+/* publish(TopicType& msg) //{ */
+
+template <class TopicType>
+void PublisherHandler_impl<TopicType>::publish(TopicType& msg) {
+
+  if (!publisher_initialized_) {
+    return;
+  }
+
+  try {
+    publisher_.publish(msg);
+  }
+  catch (...) {
+    ROS_ERROR("exception caught during publishing topic '%s'", publisher_.getTopic().c_str());
+  }
+}
+
+//}
+
+// --------------------------------------------------------------
+// |                      PublisherHandler                      |
+// --------------------------------------------------------------
+
+/* operator= //{ */
+
+template <class TopicType>
+PublisherHandler<TopicType>& PublisherHandler<TopicType>::operator=(const PublisherHandler<TopicType>& other) {
+
+  if (this == &other) {
+    return *this;
+  }
+
+  if (other.impl_) {
+    this->impl_ = other.impl_;
+  }
+
+  return *this;
+}
+
+//}
+
+/* copy constructor //{ */
+
+template <class TopicType>
+PublisherHandler<TopicType>::PublisherHandler(const PublisherHandler<TopicType>& other) {
+
+  if (other.impl_) {
+    this->impl_ = other.impl_;
+  }
+}
+
+//}
+
+/* PublisherHandler(ros::NodeHandle& nh, const std::string& address, const unsigned int &buffer_size, const bool &latch) //{ */
+
+template <class TopicType>
+PublisherHandler<TopicType>::PublisherHandler(ros::NodeHandle& nh, const std::string& address, const unsigned int &buffer_size, const bool &latch) {
+
+  impl_ = std::make_shared<PublisherHandler_impl<TopicType>>(nh, address, buffer_size, latch);
+}
+
+//}
+
+/* initialize(ros::NodeHandle& nh, const std::string& address) //{ */
+
+template <class TopicType>
+void PublisherHandler<TopicType>::initialize(ros::NodeHandle& nh, const std::string& address) {
+
+  impl_ = std::make_shared<PublisherHandler_impl<TopicType>>(nh, address);
+}
+
+//}
+
+/* publish(TopicType& msg) //{ */
+
+template <class TopicType>
+void PublisherHandler<TopicType>::publish(TopicType& msg) {
+
+  impl_->publish(msg);
+}
+
+//}
+
+}  // namespace mrs_lib
+
+#endif  // PUBLISHER_HANDLER_HPP
