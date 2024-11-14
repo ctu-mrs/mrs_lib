709edc58a6d4d158f166cbba7021f4656ece95da
Marlon Rivera <Marlonriv90@gmail.com>
2024-11-14 17:20:59 +0100

    WIP: Updated to support input of borders defined in latlon and altitudes in AMSL, only working for static edges, need to update accordingly the interactive markers

--- include/mrs_lib/safety_zone/static_edges_visualization.h
+++ include/mrs_lib/safety_zone/static_edges_visualization.h
@@ -11,6 +11,8 @@
 #include <memory>
 #include <atomic>
 
+namespace gm = geometry_msgs;
+
 namespace mrs_lib
 {
   class StaticEdgesVisualization final : public Subscriber
@@ -18,14 +20,14 @@ namespace mrs_lib
 
   public:
     // Represents the prism
-    StaticEdgesVisualization(Prism* prism, const std::string& frame_id, const ros::NodeHandle& nh, const double& markers_update_rate);
+    StaticEdgesVisualization(Prism* prism, const std::string& uav_name, const std::string& frame_id, const ros::NodeHandle& nh, const double& markers_update_rate);
 
     // Represents corresponding obstacle in the safety_zone.
-    StaticEdgesVisualization(SafetyZone* safety_zone, const int& obstacle_id, const std::string& frame_id, const ros::NodeHandle& nh,
+    StaticEdgesVisualization(SafetyZone* safety_zone, const int& obstacle_id, const std::string& uav_name, const std::string& frame_id, const ros::NodeHandle& nh,
                              const double& markers_update_rate);
 
     // Represents border of the safety_zone.
-    StaticEdgesVisualization(SafetyZone* safety_zone, const std::string& frame_id, const ros::NodeHandle& nh, const double& markers_update_rate, std::shared_ptr<mrs_lib::Transformer>& transformer);
+    StaticEdgesVisualization(SafetyZone* safety_zone, const std::string& uav_name, const std::string& frame_id, const ros::NodeHandle& nh, const double& markers_update_rate, std::shared_ptr<mrs_lib::Transformer>& transformer);
 
     ~StaticEdgesVisualization();
 
@@ -35,6 +37,7 @@ namespace mrs_lib
   private:
     void init();
     void sendMarker([[maybe_unused]] const ros::TimerEvent& event);
+    std::tuple<bool, gm::Point> transformPoint(gm::Point point, std::string& frame_id, geometry_msgs::TransformStamped tf);
 
     // It is required for generating static marker names,
     // because their id's must be unique on topic
@@ -42,6 +45,7 @@ namespace mrs_lib
     static std::atomic<int> id_generator_;
 
     Prism* prism_;
+    std::string uav_name_;
     std::string frame_id_;
     ros::NodeHandle nh_;
     std::shared_ptr<mrs_lib::Transformer> transformer_;
--- src/safety_zone/static_edges_visualization.cpp
+++ src/safety_zone/static_edges_visualization.cpp
@@ -14,8 +14,8 @@ namespace mrs_lib
 
   /* StaticEdgesVisualization(prism) //{ */
 
-  StaticEdgesVisualization::StaticEdgesVisualization(Prism* prism, const std::string& frame_id, const ros::NodeHandle& nh, const double& markers_update_rate)
-      : id_(id_generator_++), prism_(prism), frame_id_(frame_id), nh_(nh)
+  StaticEdgesVisualization::StaticEdgesVisualization(Prism* prism, const std::string& uav_name, const std::string& frame_id, const ros::NodeHandle& nh, const double& markers_update_rate)
+      : id_(id_generator_++), prism_(prism), uav_name_(uav_name), frame_id_(frame_id), nh_(nh)
   {
     timer_ = nh_.createTimer(ros::Rate(markers_update_rate), &StaticEdgesVisualization::sendMarker, this);
     last_markers_.markers.push_back(vm::Marker());
@@ -26,9 +26,9 @@ namespace mrs_lib
 
   /* StaticEdgesVisualization(obstacle) //{ */
 
-  StaticEdgesVisualization::StaticEdgesVisualization(SafetyZone* safety_zone, const int& obstacle_id, const std::string& frame_id, const ros::NodeHandle& nh,
+  StaticEdgesVisualization::StaticEdgesVisualization(SafetyZone* safety_zone, const int& obstacle_id, const std::string& uav_name, const std::string& frame_id, const ros::NodeHandle& nh,
                                                      const double& markers_update_rate)
-      : id_(id_generator_++), prism_(safety_zone->getObstacle(obstacle_id)), frame_id_(frame_id), nh_(nh)
+      : id_(id_generator_++), prism_(safety_zone->getObstacle(obstacle_id)), uav_name_(uav_name), frame_id_(frame_id), nh_(nh)
   {
     timer_ = nh_.createTimer(ros::Rate(markers_update_rate), &StaticEdgesVisualization::sendMarker, this);
     last_markers_.markers.push_back(vm::Marker());
@@ -39,9 +39,9 @@ namespace mrs_lib
 
   /* StaticEdgesVisualization(safety border) //{ */
 
-  StaticEdgesVisualization::StaticEdgesVisualization(SafetyZone* safety_zone, const std::string& frame_id, const ros::NodeHandle& nh,
+  StaticEdgesVisualization::StaticEdgesVisualization(SafetyZone* safety_zone, const std::string& uav_name, const std::string& frame_id, const ros::NodeHandle& nh,
                                                      const double& markers_update_rate, std::shared_ptr<mrs_lib::Transformer>& transformer)
-      : id_(id_generator_++), prism_(safety_zone->getBorder()), frame_id_(frame_id), nh_(nh), transformer_(transformer)
+      : id_(id_generator_++), prism_(safety_zone->getBorder()), uav_name_(uav_name), frame_id_(frame_id), nh_(nh), transformer_(transformer)
   {
     last_markers_.markers.push_back(vm::Marker());
     timer_ = nh_.createTimer(ros::Rate(markers_update_rate), &StaticEdgesVisualization::sendMarker, this);
@@ -97,10 +97,11 @@ namespace mrs_lib
     const double max_z = prism_->getMaxZ();
     const double min_z = prism_->getMinZ();
     const auto polygon = prism_->getPolygon().outer();
+    std::string target_frame_id = "world_origin";
 
     vm::Marker marker;
     marker.id = id_;
-    marker.header.frame_id = frame_id_;
+    marker.header.frame_id = uav_name_ +"/" + target_frame_id;
     marker.type = vm::Marker::LINE_LIST;
     marker.action = vm::Marker::ADD;
     marker.color.a = 0.3;
@@ -110,7 +111,71 @@ namespace mrs_lib
     marker.pose.orientation = mrs_lib::AttitudeConverter(0, 0, 0);
     marker.scale.x = 0.2;
 
+    /* old_version //{ */
+
+    /* // Iterating over polygon edges to publish the marker lines */
+    /* for (size_t i = 0; i < polygon.size() - 1; i++) */
+    /* {  // -1 because the last is the same as the first one */
+    /*   // Adding upper horizontal edges */
+    /*   gm::Point upper_start; */
+    /*   gm::Point upper_end; */
+
+    /*   upper_start.x = polygon[i].get<0>(); */
+    /*   upper_start.y = polygon[i].get<1>(); */
+    /*   upper_start.z = max_z; */
+
+    /*   upper_end.x = polygon[i + 1].get<0>(); */
+    /*   upper_end.y = polygon[i + 1].get<1>(); */
+    /*   upper_end.z = max_z; */
+
+    /*   marker.points.push_back(upper_start); */
+    /*   marker.points.push_back(upper_end); */
+
+    /*   // Adding lower horizontal edges */
+    /*   gm::Point lower_start; */
+    /*   gm::Point lower_end; */
+
+    /*   lower_start.x = polygon[i].get<0>(); */
+    /*   lower_start.y = polygon[i].get<1>(); */
+    /*   lower_start.z = min_z; */
+
+    /*   lower_end.x = polygon[i + 1].get<0>(); */
+    /*   lower_end.y = polygon[i + 1].get<1>(); */
+    /*   lower_end.z = min_z; */
+
+    /*   marker.points.push_back(lower_start); */
+    /*   marker.points.push_back(lower_end); */
+
+    /*   // Adding vertical edges */
+    /*   marker.points.push_back(upper_start); */
+    /*   marker.points.push_back(lower_start); */
+    /* } */
+
+    /* last_markers_.markers[id_] = marker; */
+    /* publisher_.publish(last_markers_); */
+
+
+    //}
+
+    /* new_version //{ */
+    // If we fail in transforming the area at some point
+    // do not publish it at all
+    bool tf_success = true;
+    auto ret = transformer_->getTransform(frame_id_, target_frame_id , ros::Time(0));
+    geometry_msgs::TransformStamped tf_viz;
+
+    if (ret)
+    {
+      ROS_INFO_ONCE("[SafetyAreaManager]: got TFs, can publish safety area markers");
+      tf_viz = ret.value();
+    } else
+    {
+      tf_success = false;
+    }
+
+
     // Iterating over polygon edges to publish the marker lines
+
     for (size_t i = 0; i < polygon.size() - 1; i++)
     {  // -1 because the last is the same as the first one
       // Adding upper horizontal edges
@@ -125,8 +190,11 @@ namespace mrs_lib
       upper_end.y = polygon[i + 1].get<1>();
       upper_end.z = max_z;
 
-      marker.points.push_back(upper_start);
-      marker.points.push_back(upper_end);
+      auto [res1, transformed_upper_start] = transformPoint(upper_start, target_frame_id, tf_viz);
+      marker.points.push_back(transformed_upper_start);
+
+      auto [res2, transformed_upper_end] = transformPoint(upper_end, target_frame_id, tf_viz);
+      marker.points.push_back(transformed_upper_end);
 
       // Adding lower horizontal edges
       gm::Point lower_start;
@@ -140,15 +208,64 @@ namespace mrs_lib
       lower_end.y = polygon[i + 1].get<1>();
       lower_end.z = min_z;
 
-      marker.points.push_back(lower_start);
-      marker.points.push_back(lower_end);
+      auto [res3, transformed_lower_start] = transformPoint(lower_start, target_frame_id, tf_viz);
+      marker.points.push_back(transformed_lower_start);
+
+      auto [res4, transformed_lower_end] = transformPoint(lower_end, target_frame_id, tf_viz);
+      marker.points.push_back(transformed_lower_end);
+
+      if (!res1 || !res2 || !res3 || !res4)
+      {
+        tf_success = false;
+      }
 
       // Adding vertical edges
-      marker.points.push_back(upper_start);
-      marker.points.push_back(lower_start);
+      marker.points.push_back(transformed_upper_start);
+      marker.points.push_back(transformed_lower_start);
     }
-    last_markers_.markers[id_] = marker;
-    publisher_.publish(last_markers_);
+
+    if(tf_success)
+    {
+      last_markers_.markers[id_] = marker;
+      publisher_.publish(last_markers_);
+
+    }
+
+    //}
+  }
+
+  //}
+
+  /* transformPoint() //{ */
+
+  std::tuple<bool, gm::Point> StaticEdgesVisualization::transformPoint(gm::Point point, std::string& frame_id, geometry_msgs::TransformStamped tf)
+  {
+
+    mrs_msgs::ReferenceStamped temp_ref;
+    gm::Point transformed_point;
+    temp_ref.header.frame_id = frame_id;
+    temp_ref.header.stamp = ros::Time(0);
+    temp_ref.reference.position.x = point.x;
+    temp_ref.reference.position.y = point.y;
+    temp_ref.reference.position.z = point.z;
+
+    ROS_INFO_STREAM("[SafetyAreaManager]:  Original point x: " << point.x << " y: " << point.y << " z: " << point.z);
+
+    if (auto ret = transformer_->transform(temp_ref, tf))
+    {
+      temp_ref = ret.value();
+      ROS_INFO_STREAM("[SafetyAreaManager]: Transformed point x: " << temp_ref.reference.position.x << " y: " << temp_ref.reference.position.y
+                                                                               << " z: " << temp_ref.reference.position.z);
+    } else
+    {
+      return std::make_tuple(false, transformed_point);
+    }
+
+    transformed_point.x = temp_ref.reference.position.x;
+    transformed_point.y = temp_ref.reference.position.y;
+    transformed_point.z = temp_ref.reference.position.z;
+
+    return std::make_tuple(true, transformed_point);
   }
 
   //}
