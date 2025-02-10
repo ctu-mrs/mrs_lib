ddf4934a8ce9752594095e2717f5bd69d8067895
Afzal Ahmad <afzalhmd14@gmail.com>
2025-02-10 14:26:20 +0100

    added: test for param_provider

--- test/run_specific_test.sh
+++ test/run_specific_test.sh
@@ -11,6 +11,6 @@ done
 
 colcon test-result --delete-yes
 
-colcon test --packages-select mrs_lib --ctest-args -R 'math'
+colcon test --packages-select mrs_lib --ctest-args -R 'param_provider'
 
 colcon test-result --all --verbose
