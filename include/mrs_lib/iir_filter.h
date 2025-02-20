1d41f253fd4e84e662ffd63a59c28d5abd0f281c
Michal Reiser <reisemic@fel.cvut.cz>
2025-02-20 09:39:44 +0100

    fixed possibly faulty constructor for IIR filter

--- include/mrs_lib/iir_filter.h
+++ include/mrs_lib/iir_filter.h
@@ -12,10 +12,19 @@ namespace mrs_lib
 class IirFilter {
 
 public:
+  /**
+   * @brief Constructor of new IirFilter object (coefficients are in the canonical form)
+   * 
+   * @param a gains in the feedback branch of the IIR filter be advised that the coefficient a[0] is not used and should be set to zero.
+   * @param b gains in the feedforward branch, using only this coefficients results in FIR filter 
+   */
   IirFilter(const std::vector<double>& a, const std::vector<double>& b);
+  IirFilter();
 
-  double iterate(const double& input);
+  double iterate(const double input);
 
+  std::tuple<std::vector<double>,std::vector<double>> getCoeffs();
+  std::vector<double> getBuffer();
 
 private:
   std::vector<double> a_;
--- src/iir_filter/iir_filter.cpp
+++ src/iir_filter/iir_filter.cpp
@@ -1,4 +1,4 @@
-#include <mrs_lib/iir_filter.h>
+#include "iir_filter.h"
 
 namespace mrs_lib
 {
@@ -12,37 +12,76 @@ IirFilter::IirFilter(const std::vector<double>& a_in, const std::vector<double>&
   a_ = a_in;
   b_ = b_in;
 
-  order_ = a_.size();
-  buffer_.resize(order_, 0.0);
-  for (size_t i = 0; i < a_.size(); i++) {
+  // get the order of the filter based on the more prominent part of the coefficients
+  order_ = std::max(a_.size(), b_.size()) - 1;
+  
+  a_.resize(order_ +1, 0.0);
+  b_.resize(order_ +1, 0.0);
+    
+  buffer_.resize(order_ + 1, 0.0);
+  for (size_t i = 0; i < order_ + 1; i++) {
     
   ROS_INFO_STREAM("a: " << a_[i] << " b: " << b_[i]);
   }
+  ROS_INFO("filter order : %d",order_);
   ROS_INFO("[%s]: IIR filter initialized!", ros::this_node::getName().c_str());
 }
 
 //}
 
+
+/* IirFilter constructor //{ */
+  
+IirFilter::IirFilter(){
+    a_.resize(1,0.0);
+    b_.resize(1,1.0);
+    order_ = 0;
+    buffer_.resize(order_ + 1, 0.0);
+  }
+
+//}
+
+
+
 /* iterate() //{ */
 
-double IirFilter::iterate(const double& input) {
-
+double IirFilter::iterate(const double input) {
   double output = 0;
 
   buffer_[0] = input;
 
-  for (size_t i = 1; i < order_; i++) {
+  for (size_t i = 1; i < order_ + 1; i++) {
     buffer_[0] += (-a_[i]) * buffer_[i];
     output += (b_[i]) * buffer_[i];
   }
 
   output += buffer_[0] * b_[0];
 
-  for (size_t i = order_ - 1; i > 0; i--) {
+  for (size_t i = order_; i > 0; i--) {
     buffer_[i] = buffer_[i - 1];
   }
 
   return output;
 }
+
+//}
+
+
+/* getCoeffs //{ */
+
+  std::tuple<std::vector<double>,std::vector<double>> IirFilter::getCoeffs(){
+
+    return std::make_tuple(a_,b_);
+  }
+
+//}
+
+/* getBuffer //{ */
+
+std::vector<double> IirFilter::getBuffer(){
+    return buffer_;
+}
+
+//}
 
 }  // namespace mrs_lib
