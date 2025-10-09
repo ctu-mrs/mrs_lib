16d9be7b3c561e52f84f04cd653b1700a3005329
matous <vrbamato@fel.cvut.cz>
2025-10-09 15:42:42 +0200

    updated the throttle model to allow for a general quadratic equation

--- include/mrs_lib/quadratic_throttle_model.h
+++ include/mrs_lib/quadratic_throttle_model.h
@@ -6,27 +6,68 @@
 namespace mrs_lib
 {
 
-namespace quadratic_throttle_model
-{
+  namespace quadratic_throttle_model
+  {
 
-typedef struct
-{
-  double A;
-  double B;
-  int    n_motors;
-} MotorParams_t;
+    struct motor_params_t
+    {
+      int    n_motors;
+      double A;
+      double B;
+      double C;
+      enum class type_t
+      {
+        legacy,
+        full_quadratic,
+      } type;
 
-double inline throttleToForce(const MotorParams_t motor_params, const double throttle) {
+      // helper pre-computed constants
+      double An;
+      double Bn;
+      double Cn;
 
-  return motor_params.n_motors * pow((throttle - motor_params.B) / motor_params.A, 2);
-}
+      motor_params_t(int n_motors, double A, double B)
+        : n_motors(n_motors), A(A), B(B), type(type_t::legacy)
+      {}
 
-double inline forceToThrottle(const MotorParams_t motor_params, const double force) {
+      motor_params_t(int n_motors, double A, double B, double C)
+        : n_motors(n_motors), A(A), B(B), C(C), type(type_t::legacy), An(A / n_motors), Bn(B / n_motors), Cn(C / n_motors)
+      {}
+    };
 
-  return sqrt(force / motor_params.n_motors) * motor_params.A + motor_params.B;
-}
+    double inline throttleToForce(const motor_params_t& motor_params, const double throttle)
+    {
+      // F = N * ( A * T * T + B * T + C )
+      if (motor_params.type == motor_params_t::type_t::full_quadratic)
+        return motor_params.n_motors * ( motor_params.A*throttle*throttle + motor_params.B*throttle + motor_params.C );
+      else
+        return motor_params.n_motors * pow((throttle - motor_params.B) / motor_params.A, 2);
+    }
 
-}  // namespace quadratic_throttle_model
+    double inline forceToThrottle(const motor_params_t& motor_params, const double force)
+    {
+      // F = N*( A*T*T + B*T + C )
+      // F = A/N * T * T + B/N * T + C/N
+      // 0 = A/N * T * T + B/N * T + ( C/N - F )
+      // A' = A/N, B' = B/N, C' = C/N - F
+      if (motor_params.type == motor_params_t::type_t::full_quadratic)
+      {
+        const double Cp = motor_params.Cn - force;
+        double sqrt_part = motor_params.Bn * motor_params.Bn - 4.0 * motor_params.An * Cp ;
+        // TODO: add some warning here
+        if (sqrt_part <= 0.0)
+          sqrt_part = 0;
+        // only the right half of the parabole is used, so only root1 is necessary
+        const double root1 = (- motor_params.Bn + std::sqrt( sqrt_part )) / (2 * motor_params.An);
+        /* const double root2 = (- motor_params.Bn - std::sqrt( motor_params.Bn * motor_params.Bn - 4.0 * motor_params.An * Cp )) / (2 * motor_params.An); */
+        return root1;
+      }
+      else
+        // the legacy computation
+        return sqrt(force / motor_params.n_motors) * motor_params.A + motor_params.B;
+    }
+
+  }  // namespace quadratic_throttle_model
 
 }  // namespace mrs_lib
 
