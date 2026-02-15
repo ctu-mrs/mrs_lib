52116d9d5f7a13d343b922a340026e161c278605
pum1k <55055380+pum1k@users.noreply.github.com>
2026-02-15 18:43:26 +0100

    Add apt-get update before install to docs script

--- .ci/docs.sh
+++ .ci/docs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+sudo apt-get update
 sudo apt-get install doxygen doxygen-latex graphviz
 
 line=$(cat CMakeLists.txt | grep "project(.*)" -o); tmp=${line:8}; proj_name=${tmp:0:${#tmp}-1};
