c3760d275d231ea8c7577951d8036fac3c6d75ba
Filip <filipmymail@gmail.com>
2026-03-19 15:19:11 +0100

    Format and comment out ROS1 MACRO

--- include/mrs_lib/quadratic_throttle_model.h
+++ include/mrs_lib/quadratic_throttle_model.h
@@ -14,12 +14,12 @@ namespace mrs_lib
     {
       motor_params_t() = default;
 
-      motor_params_t(int n_motors, double a, double b)
-        : n_motors(n_motors), a(a), b(b), c(0), type(type_t::legacy), an(0), bn(0), cn(0)
-      {}
+      motor_params_t(int n_motors, double a, double b) : n_motors(n_motors), a(a), b(b), c(0), type(type_t::legacy), an(0), bn(0), cn(0)
+      {
+      }
 
       motor_params_t(int n_motors, double a, double b, double c)
-        : n_motors(n_motors), a(a), b(b), c(c), type(type_t::full_quadratic), an(a * n_motors), bn(b * n_motors), cn(c * n_motors)
+          : n_motors(n_motors), a(a), b(b), c(c), type(type_t::full_quadratic), an(a * n_motors), bn(b * n_motors), cn(c * n_motors)
       {
         if (a < 1e-6)
           type = type_t::linear;
@@ -32,21 +32,21 @@ namespace mrs_lib
         pl.loadParam<double>("motor_params/b", b);
         type = type_t::legacy;
         const auto c_loaded = pl.loadParam("motor_params/c", c, 0.0);
-        pl.loadParam("motor_params/n_motors", n_motors );
+        pl.loadParam("motor_params/n_motors", n_motors);
 
         if (c_loaded)
         {
           type = type_t::full_quadratic;
-          an = a*n_motors;
-          bn = b*n_motors;
-          cn = c*n_motors;
+          an = a * n_motors;
+          bn = b * n_motors;
+          cn = c * n_motors;
 
           if (a < 1e-6)
             type = type_t::linear;
         }
       }
 
-      int    n_motors;
+      int n_motors;
       double a;
       double b;
       double c;
@@ -61,14 +61,13 @@ namespace mrs_lib
       double an;
       double bn;
       double cn;
-
     };
 
     double inline throttleToForce(const motor_params_t& motor_params, const double throttle)
     {
       if (motor_params.type == motor_params_t::type_t::full_quadratic || motor_params.type == motor_params_t::type_t::linear)
         // F = N*( a*T^2 + b*T + c )
-        return motor_params.n_motors * ( motor_params.a*throttle*throttle + motor_params.b*throttle + motor_params.c );
+        return motor_params.n_motors * (motor_params.a * throttle * throttle + motor_params.b * throttle + motor_params.c);
       else
         // F = N*( (T - b)/a )^2
         return motor_params.n_motors * pow((throttle - motor_params.b) / motor_params.a, 2);
@@ -84,12 +83,11 @@ namespace mrs_lib
         // T = ( F - c*N ) / ( b*N )
         if (motor_params.bn < 1e-6)
         {
-          ROS_ERROR_THROTTLE(1.0, "forceToThrottle: The N*b parameter is too small!");
+          // RCLCPP_ERROR_THROTTLE(1.0, "forceToThrottle: The N*b parameter is too small!");
           return 0.0;
         }
-        return ( force - motor_params.cn ) / motor_params.bn;
-      }
-      else if (motor_params.type == motor_params_t::type_t::full_quadratic)
+        return (force - motor_params.cn) / motor_params.bn;
+      } else if (motor_params.type == motor_params_t::type_t::full_quadratic)
       {
         // F = N*( a*T^2 + b*T + c )
         // F = a*N*^2 + b*N*T + c*N
@@ -101,21 +99,20 @@ namespace mrs_lib
         // TODO: add some warning here
         if (discriminant < 0.0)
         {
-          ROS_ERROR_THROTTLE(1.0, "forceToThrottle: Discriminant of throttle solution is negative, setting zero!");
+          // RCLCPP_ERROR_THROTTLE(1.0, "forceToThrottle: Discriminant of throttle solution is negative, setting zero!");
           discriminant = 0;
         }
         // only the right half of the parabole is used, so only root1 is necessary
-        const double root1 = (- motor_params.bn + std::sqrt( discriminant )) / (2 * motor_params.an);
+        const double root1 = (-motor_params.bn + std::sqrt(discriminant)) / (2 * motor_params.an);
         /* const double root2 = (- motor_params.bn - std::sqrt( discriminant )) / (2 * motor_params.an); */
         return std::clamp(root1, 0.0, 1.0);
-      }
-      else
+      } else
         // the legacy computation
         return sqrt(force / motor_params.n_motors) * motor_params.a + motor_params.b;
     }
 
-  }  // namespace quadratic_throttle_model
+  } // namespace quadratic_throttle_model
 
-}  // namespace mrs_lib
+} // namespace mrs_lib
 
-#endif  // QUADRATIC_THRUST_MODEL_H
+#endif // QUADRATIC_THRUST_MODEL_H
